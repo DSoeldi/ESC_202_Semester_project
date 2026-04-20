1d3d05732ae0472119e4b69097a0f508d56c9679
DSoeldi <diego.soeldi@gmail.com>
2026-04-20 15:05:34 +0200

    animation works with colors

--- anim_func.py
+++ anim_func.py
@@ -4,20 +4,28 @@ from matplotlib.animation import FuncAnimation
 
 
 # Animation function
-def animate(i, snapshots):
+def animate(i, snapshots,param_dict, ax, entities):
     ax.clear()
     coords_list = snapshots[i]  # Get the i-th snapshot
     xs, ys = zip(*coords_list)
-    ax.scatter(xs, ys)
+    color_map = {"H": "blue", "Z": "red"}
+    colors = [color_map[entity.mode] for entity in entities]
+    ax.scatter(xs, ys, c = colors)
+    ax.set_ylim(param_dict["y_bounds"])
+    ax.set_xlim(param_dict["x_bounds"])
     ax.set_title(f"Step {i}")
+    return ax
     
 
-fig, ax = plt.subplots()
-def run_animate(snapshots):
-
-    # ani = FuncAnimation(fig_anim, animate, frames = 20, interval=i, repeat = False, fargs=(entities,))
-    # return ani
-    ani = FuncAnimation(fig, animate, frames=len(snapshots), interval=200, repeat=False, fargs=(snapshots,))
+def run_animate(snapshots, param_dict, entities):
+    fig, ax = plt.subplots()
+    ani = FuncAnimation(fig, animate, frames=len(snapshots), interval=100, repeat=False, fargs=(snapshots,param_dict, ax, entities))
     return ani
 
 
+def plot_entities(entities):
+    x_list, y_list, mode = zip(*[(entity.pos[0], entity.pos[1], entity.mode) for entity in entities])
+    color_map = {"H": "blue", "Z": "red"}
+    colors = [color_map[entity.mode] for entity in entities]
+    plt.scatter(x_list, y_list, c = colors)
+    
--- run.py
+++ run.py
@@ -6,19 +6,30 @@ from step_update import *
 from anim_func import *
 
 xbounds = (0.0,10.0)
-ybounds = (0,10)
+ybounds = (0.,10.)
 
-param_dict = create_parameter_dict(n_H=10, n_Z=10,timestep=0.1, n_steps=100, 
+param_dict = create_parameter_dict(n_H=10, n_Z=10,timestep=1., n_steps=100, 
                                    x_bounds=np.array(xbounds), y_bounds=np.array(ybounds))
 
 entities = Initialize_entities(param_dict)
+
+plt.figure()
+plot_entities(entities)
+plt.savefig("start.png")
+plt.close()
+
 root_cell = Initialize_root_cell(param_dict, entities)
 param_dict["root_cell"] = root_cell
 snapshots = []
 for step in range(0,param_dict["n_steps"]):
     step_update(entities, root_cell, param_dict)
     print(step)
-    snapshots.append([entity.pos for entity in entities])  # Store a copy of 
+    snapshots.append([entity.pos.copy() for entity in entities])  # Store a copy of 
 
-ani = run_animate(snapshots)
-plt.show()
+plt.figure()
+plot_entities(entities)
+plt.savefig("end.png")
+plt.close()
+
+ani = run_animate(snapshots, param_dict, entities)
+ani.save("testanimation.gif")
