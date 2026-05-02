dd29c4353b0de820e3ad9e0a161281f2c65dbeaa
DSoeldi <diego.soeldi@gmail.com>
2026-05-02 12:03:41 +0200

    fixed bug in animation

--- run.py
+++ run.py
@@ -15,9 +15,9 @@ param_dict = create_parameter_dict(n_H=100, n_Z=20,timestep=.008, n_steps=50,
                                    max_speed_Z = 20., max_speed_H = 26.,
                                    awareness_r_Z = 0.8,awareness_r_H = 0.9,
                                    x_bounds=np.array(xbounds), y_bounds=np.array(ybounds), 
-                                   flocking_factors=(0.2,0.3,0.2)
-                                   )
+                                   flocking_factors=(0.2,0.3,0.2),
+                                   analyze=True)
 np.random.seed(42)
-snapshots = run(param_dict)
+snapshots, analyze = run(param_dict)
 ani = run_animate(snapshots, param_dict)
 ani.save("outputs/more_centering_factor_animation.gif")
